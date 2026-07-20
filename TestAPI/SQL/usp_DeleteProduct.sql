3a8543e6a2a34f8a3098c3dd76f1c919d017c0d2
chaminda <chaminda@DESKTOP-B8ELCVI>
2026-07-20 10:53:59 +0530

    Fix DELETE 404: check row existence before soft-delete to avoid @@ROWCOUNT=0 on no-op UPDATE

--- TestAPI/SQL/usp_DeleteProduct.sql
+++ TestAPI/SQL/usp_DeleteProduct.sql
@@ -4,10 +4,15 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    UPDATE Products
-    SET IsActive = 0, UpdatedAt = GETUTCDATE()
-    WHERE Id = @Id;
+    IF NOT EXISTS (SELECT 1 FROM Products WHERE Id = @Id)
+        SELECT 0 AS RowsAffected;
+    ELSE
+    BEGIN
+        UPDATE Products
+        SET IsActive = 0, UpdatedAt = GETUTCDATE()
+        WHERE Id = @Id;
 
-    SELECT @@ROWCOUNT AS RowsAffected;
+        SELECT 1 AS RowsAffected;
+    END
 END
 GO
